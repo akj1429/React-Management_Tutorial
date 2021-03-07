5601535ee45036c9afaf2fd8256e02075be9481d
akj1429 <akj14291@gmail.com>
2021-03-08 00:52:24 +0900

    first test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="gray-background">
       <img src={logo} lat="logo" />
       <h2>Let's develop management system!</h2>
-
+      <div>깃허브 연동 테스트</div>
     </div>
   );
 }
